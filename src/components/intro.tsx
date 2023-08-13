d7c8807ae23e06c538323f32a192e9b303435e63
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2023-08-13 22:36:47 +0530

    fix: cursor improved

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -22,17 +22,17 @@ const dynamicTextRef = useRef<HTMLSpanElement | null>(null);
               if (!isDeleting && charIndex < currentWord.length) {
                 // If condition is true, type the next character
                 charIndex++;
-                setTimeout(typeEffect, 200);
+                setTimeout(typeEffect, 100);
               } else if (isDeleting && charIndex > 0) {
                 // If condition is true, remove the previous character
                 charIndex--;
-                setTimeout(typeEffect, 100);
+                setTimeout(typeEffect, 10);
               } else {
                 // If word is deleted then switch to the next word
                 isDeleting = !isDeleting;
                 dynamicText!.classList.remove("stop-blinking");
                 wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
-                setTimeout(typeEffect, 1200);
+                setTimeout(typeEffect, 50);
               }
           }
           
